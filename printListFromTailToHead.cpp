02ea4756c95f219a9932ad056f320926db32e3e3
Haiman Bian <bhm2164@163.com>
2016-07-30 09:45:23 +0800

    Update printListFromTailToHead.cpp

--- printListFromTailToHead.cpp
+++ printListFromTailToHead.cpp
@@ -9,3 +9,29 @@
 输出描述:
 输出为需要打印的“新链表”的表头
 */
+
+
+/**
+*  struct ListNode {
+*        int val;
+*        struct ListNode *next;
+*        ListNode(int x) :
+*              val(x), next(NULL) {
+*        }
+*  };
+*/
+class Solution {
+public:
+    vector<int> printListFromTailToHead(struct ListNode* head) {
+      if(head == NULL)
+        return NULL;
+      vector<int> res;
+      ListNode *p = head;
+      while(p)
+      {
+        res.push_back(res.begin(),p->val);
+        p = p->next;
+      }
+      return res;
+    }
+};
